379039dda126d45585d0f77cda81ff4227cf9e5b
MrYahya18 <88489038+MrYahya18@users.noreply.github.com>
2021-09-26 21:23:39 +0500

    Update Snake_game.py

--- Snake_game.py
+++ Snake_game.py
@@ -1,121 +1,170 @@
+# Steps that involve in it are:
+# 1. Installing Pygame
+# 2. Create the Screen
+# 3. Create the Snake
+# 4. Displaying the Score
+# 5. Moving the Snake
+# 6. Adding the Food
+# 7. Increasing the Length of the Snake
+# 8. Game Over when Snake hits the boundaries
+
 import pygame
 from pygame.locals import *
-import time
 import random
 
-# Initialize
+# Initialize pygame
 pygame.init()
 
-# Colors scheme
+# Window axis
+window_x = 800
+window_y = 560
+
+# Set size of block (default size of food and snake rectangle) and snake speed
+block = 10
+snake_speed = 15
+
+# Colors Scheme
 white = (255, 255, 255)
 black = (0, 0, 0)
-Green = (55, 98, 87)
-# Define Default Font to Use
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
+orange = (245, 146, 66)
+yellow = (255, 255, 102)
+red = (245, 66, 66)
+blue = (66, 90, 245)
 
-clock = pygame.time.Clock()
+# Initialize Window display mode and title
+game_window = pygame.display.set_mode(size=(window_x, window_y))
+game_window.fill(orange)
+game_title = pygame.display.set_caption("Snake Game")
 
-# Create surface
-surface_x= 780
-surface_y = 400
-game_surface = pygame.display.set_mode((surface_x, surface_y))
-game_title = pygame.display.set_caption("Snake-Game")
+# Fps Controller
+fps = pygame.time.Clock()
+
+
+# Create a function to print message .
+def message(msg, color, window_pos=[window_x / 4, window_y / 2]):
+    my_font = pygame.font.SysFont("high tower", 30)
+    mesg = my_font.render(msg, True, color)
+    game_window.blit(mesg, window_pos)
+
+
+# Function to display score
+def Score(score, position=(0, 0)):
+    score_font = pygame.font.SysFont("times new roman", 20)
+    value = score_font.render("Score: " + str(score), True, blue)
+    game_window.blit(value, position)  # Draw score surface on Window Surface
+
 
 # Define a function to create a snake
-snake_block = 10
-def snake(snake_block, snake_body):
+def snake(block, snake_body):
     for x in snake_body:
-        pygame.draw.rect(game_surface, black, [x[0], x[1], snake_block, snake_block])
+        pygame.draw.rect(game_window, black, [x[0], x[1], block, block])
 
-# Create a function to Create message when Game is over
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    game_surface.blit(mesg, [surface_x/2, surface_y/2])
+# Define a loop (Running of game till end of it, with controllers to move snake and boundary conditions)
 def gameloop():
     # Snake Position on Window Screen
-    snake_x = surface_x/2
-    snake_y = surface_y/2
+    snake_pos = [window_x / 2, window_y / 2]
 
-    # food Random position on Window Screen
-    food_x = random.randrange(1, (surface_x // 10)) * 10
-    food_y = random.randrange(1, (surface_y // 10)) * 10
-
-    running = True
-    game_close = False
-
-    x1_change = 0
-    y1_change = 0
-    # Create list to define process when snake eat block and it's length increases
+    # food position on Window Screen
+    food = [random.randrange(0, (window_x // 10)) * 10,
+            random.randrange(0, (window_y // 10)) * 10]
+    # Set initial direction (when game start snake move towards Left) and conditions
+    direction = 'LEFT'
+    change_to = direction
+    # Create list to define process when snake eat food and it's length increases
     snake_body = []
+    # Initial length of snake
     length_of_snake = 1
-    while running:
-        while game_close == True:
-            game_surface.fill(Green)
-            message("You Lose! Press C-Play Again or Q-QUit", black)
-            pygame.display.update()
-            
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_q:
-                        running = False
-                        game_close = False
-                    if event.key == K_c:
-                        gameloop()
+    # Main Logic
+    game_close = False
+    game_over = False
+    # Set controllers to move snake
+    while not game_close:
         for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
+            # Whenever a key is pressed down
             if event.type == pygame.KEYDOWN:
+                if event.key == K_UP:
+                    change_to = 'UP'
+                if event.key == K_DOWN:
+                    change_to = 'DOWN'
                 if event.key == K_LEFT:
-                    x1_change = -10
-                    y1_change = 0
-                elif event.key == K_RIGHT:
-                    x1_change = 10
-                    y1_change = 0
-                elif event.key == K_UP:
-                    y1_change = -10
-                    x1_change = 0
-                elif event.key == K_DOWN:
-                    y1_change = 10
-                    x1_change = 0
-                elif event.key == K_ESCAPE:
-                    running = False
-        snake_x += x1_change
-        snake_y += y1_change
-        game_surface.fill(Green)
+                    change_to = 'LEFT'
+                if event.key == K_RIGHT:
+                    change_to = 'RIGHT'
+                # Esc -> Create event to quit the game
+                if event.key == K_ESCAPE:
+                    game_close = True
+            # Can close game whenever we want using Quit (X)
+            if event.type == QUIT:
+                game_close = True
+        # Making sure the snake cannot move in the opposite direction instantaneously
+        if change_to == 'UP' and direction != 'DOWN':
+            direction = 'UP'
+        if change_to == 'DOWN' and direction != 'UP':
+            direction = 'DOWN'
+        if change_to == 'LEFT' and direction != 'RIGHT':
+            direction = 'LEFT'
+        if change_to == 'RIGHT' and direction != 'LEFT':
+            direction = 'RIGHT'
+
+        # Moving the snake
+        if direction == 'UP':
+            snake_pos[1] -= 10
+        if direction == 'DOWN':
+            snake_pos[1] += 10
+        if direction == 'LEFT':
+            snake_pos[0] -= 10
+        if direction == 'RIGHT':
+            snake_pos[0] += 10
+        # Whenever snake moves the most recent position of it on window will filled
+        game_window.fill(orange)
         # Create Food
-        pygame.draw.rect(game_surface,white, [food_x, food_y, 8, 8])
+        pygame.draw.rect(game_window, white, [food[0], food[1], block, block])
 
         snake_head = []
-        snake_head.append(snake_x)
-        snake_head.append(snake_y)
+        snake_head.append(snake_pos[0])
+        snake_head.append(snake_pos[1])
         snake_body.append(snake_head)
-        if len(snake_head) > length_of_snake:
+        if len(snake_body) > length_of_snake:
             del snake_body[0]
 
         for x in snake_body[:-1]:
             if x == snake_head:
                 game_close = True
 
-        snake(snake_block, snake_body)
+        # Create Snake
+        snake(block, snake_body)
+        Score(length_of_snake - 1)  # Count Scores
 
-        # Refresh game screen
-        pygame.display.update()
-
-        if snake_y == food_x and snake_y == food_y:
-            food_x = random.randrange(1, (surface_x // 10)) * 10
-            food_y = random.randrange(1, (surface_y // 10)) * 10
+        if snake_pos[0] == food[0] and snake_pos[1] == food[1]:
+            food = [random.randrange(0, (window_x // 10)) * 10, random.randrange(0, (window_y // 10)) * 10]
             length_of_snake += 1
-        clock.tick(10)
 
-        if snake_x >= surface_x or snake_x < 0 or snake_y >= surface_y or snake_y < 0:
-            # message("Game Over", black)
-            game_close = True
-            # time.sleep(0.1)
+        # Boundary Condition (if snake touch any of boundary, game over)
+        if snake_pos[0] >= window_x or snake_pos[0] < 0 or snake_pos[1] >= window_y or snake_pos[1] < 0:
+            game_over = True
+        # Define message (when game will over and set condition to either play again or Quit)
+        while game_over == True:
+            game_window.fill(orange)
+            message("Game Over Press P-Play Again or Q-Quit", black)
+            message(f"Total Score: {length_of_snake-1}", red, window_pos=[window_x/2.5, window_y-50])
+            Score(length_of_snake - 1)
             pygame.display.update()
 
-
-# Keep Window open until desired time
+            # Set keys to Play Again or QUit when game over
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_q:
+                        game_close = True
+                        game_over = False
+                    if event.key == K_p:
+                        gameloop()
+        # Refresh game screen
+        pygame.display.update()
+        # Cam use to define fps as or can say speed of snake
+        fps.tick(snake_speed)
+    # Keep Window open until desired time
     pygame.quit()
     quit()
-gameloop()
+
+
+Game = gameloop()
